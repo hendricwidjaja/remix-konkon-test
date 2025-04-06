2cdc6ad875c11bbe6f2d9e3e302a946fcf345c45
hendricwidjaja <hcwidjaja@gmail.com>
2025-04-06 23:38:49 +1000

    feat: adjust email input styling

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -87,7 +87,7 @@ export default function Hero({
           name="email"
           placeholder="Your email"
           required
-          className="font-ocr max-w-md w-10/12 sm:w-72 px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 bg-gray-800 border-gray-500 text-gray-100"
+          className="font-ocr max-w-md w-10/12 sm:w-72 px-4 py-2 border rounded-lg focus:outline-none focus:ring-1 focus:ring-aquaKonkon bg-gray-800 border-gray-500 text-gray-100"
           disabled={loading} // Disable input while loading
         />
         <button
